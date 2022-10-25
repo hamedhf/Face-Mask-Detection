22d7a61e422f027e1bba040730e565bec3a17b5a
Hamed Feizabadi <hamedfz19@gmail.com>
2022-10-25 11:47:34 +0330

    update

--- detect_mask_image.py
+++ detect_mask_image.py
@@ -133,50 +133,53 @@ def mask_detector(input_image = None):
 
 	main_label = 'No mask'
 	# loop over the detections
-	for i in range(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the detection
-		confidence = detections[0, 0, i, 2]
+	try:
+		for i in range(0, detections.shape[2]):
+			# extract the confidence (i.e., probability) associated with
+			# the detection
+			confidence = detections[0, 0, i, 2]
 
-		# filter out weak detections by ensuring the confidence is
-		# greater than the minimum confidence
-		if confidence > args["confidence"]:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the object
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
+			# filter out weak detections by ensuring the confidence is
+			# greater than the minimum confidence
+			if confidence > args["confidence"]:
+				# compute the (x, y)-coordinates of the bounding box for
+				# the object
+				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+				(startX, startY, endX, endY) = box.astype("int")
 
-			# ensure the bounding boxes fall within the dimensions of
-			# the frame
-			(startX, startY) = (max(0, startX), max(0, startY))
-			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+				# ensure the bounding boxes fall within the dimensions of
+				# the frame
+				(startX, startY) = (max(0, startX), max(0, startY))
+				(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-			# extract the face ROI, convert it from BGR to RGB channel
-			# ordering, resize it to 224x224, and preprocess it
-			face = image[startY:endY, startX:endX]
-			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-			face = cv2.resize(face, (224, 224))
-			face = img_to_array(face)
-			face = preprocess_input(face)
-			face = np.expand_dims(face, axis=0)
+				# extract the face ROI, convert it from BGR to RGB channel
+				# ordering, resize it to 224x224, and preprocess it
+				face = image[startY:endY, startX:endX]
+				face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+				face = cv2.resize(face, (224, 224))
+				face = img_to_array(face)
+				face = preprocess_input(face)
+				face = np.expand_dims(face, axis=0)
 
-			# pass the face through the model to determine if the face
-			# has a mask or not
-			(mask, withoutMask) = model.predict(face)[0]
+				# pass the face through the model to determine if the face
+				# has a mask or not
+				(mask, withoutMask) = model.predict(face)[0]
 
-			# determine the class label and color we'll use to draw
-			# the bounding box and text
-			label = "Mask" if mask > withoutMask else "No Mask"
-			color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+				# determine the class label and color we'll use to draw
+				# the bounding box and text
+				label = "Mask" if mask > withoutMask else "No Mask"
+				color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 
-			# include the probability in the label
-			main_label = label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+				# include the probability in the label
+				main_label = label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-			# display the label and bounding box rectangle on the output
-			# frame
-			cv2.putText(image, label, (startX, startY - 10),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-			cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
+				# display the label and bounding box rectangle on the output
+				# frame
+				cv2.putText(image, label, (startX, startY - 10),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+				cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
+	except Exception:
+		print("Error in mask detection. Returning No mask label.")
 
 	# show the output image
 	# cv2.imshow("Output", image)
